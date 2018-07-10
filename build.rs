eb010d44ecad90af1244937b0e01f932c40b2169
Robby Madruga <robbymadruga@gmail.com>
2018-07-09 18:13:32 -0700

    Added cmake build step

--- build.rs
+++ build.rs
@@ -1,8 +1,18 @@
+extern crate cmake;
 extern crate bindgen;
 
-use std::env;
-use std::path::PathBuf;
+use cmake::Config;
 
 fn main() {
-    
+    Config::new("ChakraCore")
+        .define("CMAKE_CXX_COMPILER", "/usr/bin/clang++")
+        .define("CMAKE_C_COMPILER", "/usr/bin/clang")
+        .define("ICU_SETTINGS_RESET", "1")
+        .define("STATIC_LIBRARY_SH", "1")
+        .define("CC_USES_SYSTEM_ARCH_SH", "1")
+        .define("CMAKE_BUILD_TYPE", "Debug")
+        .define("INTL_ICU_SH", "1")
+        .define("CMAKE_EXPORT_COMPILE_COMMANDS", "ON")
+        .build_target("")
+        .build();
 }
