e7247a8888267d6ac3ca258142bdd01b5216e44f
Robby Madruga <robbymadruga@gmail.com>
2018-07-09 19:49:28 -0700

    Added bindgen step

--- build.rs
+++ build.rs
@@ -1,10 +1,21 @@
-extern crate cmake;
 extern crate bindgen;
+extern crate cmake;
 
 use cmake::Config;
 
+use std::process::Command;
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
-    Config::new("ChakraCore")
+    Command::new("python")
+        .arg("./ChakraCore/bin/ch/jstoc.py")
+        .arg("./ChakraCore/bin/ch/DbgController.js")
+        .arg("controllerScript")
+        .output()
+        .unwrap();
+
+    let mut path = Config::new("ChakraCore")
         .define("CMAKE_CXX_COMPILER", "/usr/bin/clang++")
         .define("CMAKE_C_COMPILER", "/usr/bin/clang")
         .define("ICU_SETTINGS_RESET", "1")
@@ -15,4 +26,16 @@ fn main() {
         .define("CMAKE_EXPORT_COMPILE_COMMANDS", "ON")
         .build_target("")
         .build();
+
+    path.push("build/include/ChakraCore.h");
+    
+    let bindings = bindgen::Builder::default()
+        .header(path.to_str().unwrap())
+        .generate()
+        .unwrap();
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
 }
